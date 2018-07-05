2a93514cc175945db83de4e9050004011f632796
Menuflorian <40692712+Menuflorian@users.noreply.github.com>
2018-07-05 21:47:29 +0200

    mise au propre
    mise au propre

--- models/json_utils.js
+++ models/json_utils.js
@@ -1,27 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-
-function tryParseJSON (jsonString){
-    try {
-        var o = JSON.parse(jsonString);
-        if (o && typeof o === "object") {
-            return o;
-        }
+function tryParseJSON(jsonString) {
+  try {
+    var o = JSON.parse(jsonString);
+    if (o && typeof o === "object") {
+      return o;
     }
-    catch (e) { }
-    return null;
+  } catch (e) {}
+  return null;
 }
 
+module.exports.safeJSONParse = tryParseJSON;
 
-module.exports.safeJSONParse =tryParseJSON;
-
-encode = module.exports.encodeJSON =  function(obj) {
-    obj = JSON.stringify(obj);
-    return encodeURI(obj);
+encode = module.exports.encodeJSON = function(obj) {
+  obj = JSON.stringify(obj);
+  return encodeURI(obj);
 };
 
 decode = module.exports.decodeJSON = function(obj) {
-    obj = decodeURI(obj);
-    return tryParseJSON(obj);
+  obj = decodeURI(obj);
+  return tryParseJSON(obj);
 };
