64ca1992655f8ef01c5ac8fb848e3d5cc421ae38
Menuflorian <40692712+Menuflorian@users.noreply.github.com>
2018-07-13 06:45:54 +0200

    Maj administraton
    Rajout d'un paramètre validated user
    Ajout d'un bouton pour passé un administrateur admin ou le retirer

--- routes/admin.js
+++ routes/admin.js
@@ -130,9 +130,6 @@ router.post('/admin-change-password:id', ensureAuthenticated, ensureAdmin, funct
   var id = req.params.id;
   var password = req.body.password;
   var password2 = req.body.password2;
-  console.log(id);
-  console.log(password);
-  console.log(password2);
   User.findById({
       _id: id
     },
@@ -142,13 +139,11 @@ router.post('/admin-change-password:id', ensureAuthenticated, ensureAdmin, funct
       }
         if (password != password2) {
           req.flash('error_msg', "New password don't match with cofirm password");
-          console.log('toto');
           res.render('admin-change-password', {
             layout: 'layout3'
           });
         } else {
           db_user.password = bcrypt.hashSync(password, 10);
-          console.log('tata');
           db_user.save(function(err) {
             if (err) {
               res.send(err);
@@ -163,6 +158,68 @@ router.post('/admin-change-password:id', ensureAuthenticated, ensureAdmin, funct
   );
 });
 
+//validate users
+router.post('/admin-validate-user:id', ensureAuthenticated, function(req, res) {
+  var id = req.params.id;
+  User.findById({
+      _id: id
+    },
+    function(err, db_user) {
+      if (err) {
+        res.send(err);
+      }
+      db_user.validated = true;
+      db_user.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+      req.flash('success_msg', 'User is now validated');
+      res.redirect('/admin/userlist');
+    });
+    });
+});
+
+//Make a user ans admin
+router.post('/admin-makeadmin-user:id', ensureAuthenticated, function(req, res) {
+  var id = req.params.id;
+  User.findById({
+      _id: id
+    },
+    function(err, db_user) {
+      if (err) {
+        res.send(err);
+      }
+      db_user.isAdmin = true;
+      db_user.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+        req.flash('success_msg', 'User is now an admin');
+        res.redirect('/admin/userlist');
+      });
+    });
+});
+
+//Remove admin
+router.post('/admin-removeadmin-user:id', ensureAuthenticated, function(req, res) {
+  var id = req.params.id;
+  User.findById({
+      _id: id
+    },
+    function(err, db_user) {
+      if (err) {
+        res.send(err);
+      }
+      db_user.isAdmin = false;
+      db_user.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+        req.flash('success_msg', 'This user is not longer an admin');
+        res.redirect('/admin/userlist');
+      });
+    });
+});
 
 // Delete a project
 router.post('/admin-delete-user:id', ensureAuthenticated,ensureAdmin, function(req, res) {
