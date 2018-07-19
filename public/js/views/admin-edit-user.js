25a4abede4ca09e9bb3a25116b433ee6efed4178
Maxime Quiblier <maximequiblier@gmail.com>
2018-07-19 19:49:06 +0200

    Added a class for margin between the buttons

--- public/js/views/admin-edit-user.js
+++ public/js/views/admin-edit-user.js
@@ -1,6 +1,6 @@
 var swalWithBootstrapButtons = swal.mixin({
-    confirmButtonClass: 'btn btn-danger',
-    cancelButtonClass: 'btn btn-primary',
+    confirmButtonClass: 'btn btn-danger swal-btn-last',
+    cancelButtonClass: 'btn btn-primary swal-btn-not-last',
     buttonsStyling: false,
 });
 
